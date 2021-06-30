6ae611ac193daa6ec65945e3a828dfd4015e6ba4
NicknameSC <bookgame8877@gmail.com>
2021-06-30 16:25:28 -0700

    Update API_Backpack.cs

--- Replay Editor UI/ReplayUtils/API/API_Backpack.cs
+++ Replay Editor UI/ReplayUtils/API/API_Backpack.cs
@@ -10,7 +10,7 @@ namespace Replay_Editor_UI.ReplayUtils.API
 
         static public string Get_CP_From_ID(string CosmeticID)
         {
-            string raw_json = requests.DownloadString("https://benbotfn.tk/api/v1/assetProperties?path=" + CosmeticID + "&lang=en");
+            string raw_json = requests.DownloadString("https://benbot.app/api/v1/assetProperties?path=" + CosmeticID + "&lang=en");
             dynamic parsed = JObject.Parse(raw_json);
             string CP = parsed.export_properties[0].CharacterParts[0];
             return CP;
